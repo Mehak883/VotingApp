0b5862edd6f5d349e5dfa765f92e24ee67d16845
mehak <dhawanmehak10@gmail.com>
2025-03-10 00:58:24 +0530

    update data response format & add get all and update api (Party)

--- VotingApp.API/Services/Interfaces/IStateService.cs
+++ VotingApp.API/Services/Interfaces/IStateService.cs
@@ -7,7 +7,7 @@ namespace VotingApp.API.Services.Interfaces
         Task<List<StateResponse>> GetAllStatesAsync();
         Task<StateResponse> GetStateByIdAsync(Guid id);
         Task<StateResponse> AddStateAsync(StateRequest stateRequest);
-        Task<bool> UpdateStateAsync(Guid Id, StateRequest stateRequest);
+        Task<bool?> UpdateStateAsync(Guid Id, StateRequest stateRequest);
         //Task<bool> DeleteStateAsync(Guid Id);
     }
 }
