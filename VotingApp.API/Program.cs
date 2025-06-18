f485275bbd5babfcff339fbc0ca230159472e5bf
Mehak883 <108986900+Mehak883@users.noreply.github.com>
2025-06-18 16:25:46 +0530

    Update Program.cs

--- VotingApp.API/Program.cs
+++ VotingApp.API/Program.cs
@@ -132,11 +132,10 @@ var app = builder.Build();
            
 
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionHandlerMiddleware>();
